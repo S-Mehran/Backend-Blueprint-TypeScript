70bef4520487655fd5a93f1cbe009cd7431f0e00
Syed Muhammad Mehran <smehran.mme@gmail.com>
2025-10-08 21:18:15 +0500

    replaced compareSync and hashSync with async functions

--- src/helpers/encrypt.helper.development.ts
+++ src/helpers/encrypt.helper.development.ts
@@ -1,25 +1,21 @@
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
-import * as dotenv from 'dotenv'
-
-dotenv.config()
-
-const {JWT_SECRET, SALT_ROUNDS} = process.env
-
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import * as dotenv from "dotenv";
+dotenv.config();
+const { JWT_SECRET = "", SALT_ROUNDS } = process.env;
 export default class Encrypt {
-
+ 
   static async hashPassword(password: string): Promise<string> {
-        const salt = await bcrypt.genSalt(Number(SALT_ROUNDS))
-        const hashing = await bcrypt.hash(password, salt)
-        return hashing
-    }
+    const salt = await bcrypt.genSalt(Number(SALT_ROUNDS));
+    return bcrypt.hashSync(password, salt);
+  }
+
   static async comparePassword(
     password: string,
     hashedPassword: string
   ): Promise<boolean> {
     console.log(password, hashedPassword)
-    const result = await bcrypt.compare(password, hashedPassword)
-    return result;
+    return bcrypt.compareSync(password, hashedPassword);
   }
 
   static async generateToken(payload: any): Promise<string> {
